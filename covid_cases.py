9702e9603a29e5325f67b787faf8a53cb451ca9c
csb6 <cblakley15@gmail.com>
2021-10-05 23:57:01 -0700

    Factored out common median-class code into separate module

--- covid_cases.py
+++ covid_cases.py
@@ -1,4 +1,5 @@
 import csv, datetime
+from binary_classes import classify_by_median
 
 # Population data
 
@@ -118,29 +119,6 @@ def normalize_by_population(state_populations, counts_by_date_and_state):
             states_to_counts[state_code] /= quotient
             states_to_counts[state_code] = round(states_to_counts[state_code], 3)
 
-def classify_by_median(counts_by_date_and_state):
-    counts_by_date_and_class = {}
-    for date, states_to_counts in counts_by_date_and_state.items():
-        rows = list(states_to_counts.values())
-        rows.sort()
-        daily_median = median(rows)
-        above_median = set()
-        below_median = set()
-        for state_code, count in states_to_counts.items():
-            if count >= daily_median:
-                above_median.add(state_code)
-            else:
-                below_median.add(state_code)
-        counts_by_date_and_class[date] = (below_median, above_median)
-    return counts_by_date_and_class
-
-def median(sorted_rows):
-    mid = len(sorted_rows) // 2
-    if len(sorted_rows) % 2 == 0:
-        return (sorted_rows[mid] + sorted_rows[mid-1]) / 2
-    else:
-        return sorted_rows[mid]
-
 def main():
     state_populations = get_state_populations()
     covid_data = []
@@ -155,7 +133,7 @@ def main():
             new_cases = int(row[New_Cases_Col])
             covid_data.append((date, row[State_Col], new_cases))
 
-        covid_data.sort()
+    covid_data.sort()
 
     covid_counts_by_date_and_state = by_date_and_state(covid_data)
     normalize_non_states(state_populations, covid_counts_by_date_and_state)
