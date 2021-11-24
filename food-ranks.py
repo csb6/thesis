7b4fe4a4c9c40f38d93d8dadd754c14b91f598b1
csb6 <cblakley15@gmail.com>
2021-11-23 23:04:10 -0700

    Improved food-ranks script and added a bunch more food words found from word vectors

--- food-ranks.py
+++ food-ranks.py
@@ -11,7 +11,7 @@ Tf_Log_Idf_Log_Col = 9
 
 healthy_foods, neutral_foods, unhealthy_foods = [set(x) for x in parser.get_food_scores("food_scores.txt")]
 
-def get_term_ranking(filename, col_num, cnvt_fn, tf_threshold=0):
+def get_term_ranking(filename, col_num, cnvt_fn, tf_threshold=5):
     term_value_list = []
     term_to_rank = {}
     with open(filename) as input_file:
@@ -21,76 +21,89 @@ def get_term_ranking(filename, col_num, cnvt_fn, tf_threshold=0):
             value = cnvt_fn(row[col_num])
             if int(row[Tf_Plain_Col]) < tf_threshold:
                 continue
+            elif term not in healthy_foods and term not in neutral_foods \
+                 and term not in unhealthy_foods:
+                continue
             term_value_list.append((term, value))
-    term_value_list.sort(key=lambda a: a[1], reverse=True)
+    # Sort in descending order by the chosen statistic's value
+    term_value_list.sort(key=lambda term_value: term_value[1], reverse=True)
+    # Rank (from highest value to lowest value for the statistic) each term
     for rank, (term, _) in enumerate(term_value_list, start=1):
         term_to_rank[term] = rank
     return term_value_list, term_to_rank
+
+def by_rank_diff(data):
+    term, orig_rank, new_rank = data
+    return new_rank - orig_rank
 
 def get_terms_by_rank_diff(term_to_rank_1, term_to_rank_2):
     term_rank_diff_list = []
     for term, orig_rank in term_to_rank_1.items():
         if term not in term_to_rank_2:
             continue
-        rank_diff = term_to_rank_2[term] - orig_rank
-        term_rank_diff_list.append((term, rank_diff, orig_rank))
-    term_rank_diff_list.sort(key=lambda a: a[1])
+        new_rank = term_to_rank_2[term]
+        term_rank_diff_list.append((term, orig_rank, new_rank))
+    term_rank_diff_list.sort(key=by_rank_diff)
     return term_rank_diff_list
 
-def filter_by_food_words(healthy_foods, neutral_foods, unhealthy_foods, term_rank_diff_list):
-    for term, rank_diff, orig_rank in term_rank_diff_list:
-        if term in healthy_foods or term in unhealthy_foods or term in neutral_foods:
-            yield term, rank_diff, orig_rank
-
-def get_biggest_rank_change_terms(col_num, cnvt_fn, n):
-    threshold = 120
-    term_value_list_2018, term_to_rank_2018 = get_term_ranking("tf-idf-2018-3-1-thru-2019-9-1.txt", col_num, cnvt_fn, tf_threshold=threshold)
-    term_value_list_2020, term_to_rank_2020 = get_term_ranking("tf-idf-2020-3-1-thru-2021-9-1.txt", col_num, cnvt_fn, tf_threshold=threshold)
-
-    term_rank_diff_list = get_terms_by_rank_diff(term_to_rank_2018, term_to_rank_2020)
-    last = term_rank_diff_list[-n:]
-    last.sort(key=lambda a: a[1], reverse=True)
-    return term_rank_diff_list[:n], last
-
-def get_food_words_rank_change_terms(col_num, cnvt_fn):
-    threshold = 120
-    term_value_list_2018, term_to_rank_2018 = get_term_ranking("tf-idf-2018-3-1-thru-2019-9-1.txt", col_num, cnvt_fn, tf_threshold=threshold)
-    term_value_list_2020, term_to_rank_2020 = get_term_ranking("tf-idf-2020-3-1-thru-2021-9-1.txt", col_num, cnvt_fn, tf_threshold=threshold)
-
-    term_rank_diff_list = get_terms_by_rank_diff(term_to_rank_2018, term_to_rank_2020)
-    return [(term, rank, orig_rank) for term, rank, orig_rank in term_rank_diff_list \
-            if term in healthy_foods \
-            or term in neutral_foods \
-            or term in unhealthy_foods]
-
-def print_rank_change(results, label1, label2):
-    print(label1, len(results), "terms by change in rank by", label2,
+def print_ranks(results, label):
+    print("Terms by change in rank by", label,
           "in 2018-3-1 to 2019-9-1 vs. 2020-3-1 to 2021-9-1")
-    for i, (term, rank_diff, orig_rank) in enumerate(results, start=1):
-        print(i, term, rank_diff, orig_rank)
+    for term, orig_rank, new_rank in results:
+        if term in healthy_foods:
+            kind = "healthy"
+        elif term in neutral_foods:
+            kind = "neutral"
+        else:
+            kind = "unhealthy"
+        print(term, kind, orig_rank, new_rank)
+
+def get_stats(terms_by_rank_diff):
+    healthy_food_sum = 0
+    healthy_food_count = 0
+    neutral_food_sum = 0
+    neutral_food_count = 0
+    unhealthy_food_sum = 0
+    unhealthy_food_count = 0
+    for term, orig_rank, new_rank in terms_by_rank_diff:
+        diff = new_rank - orig_rank # orig_rank + diff = new_rank
+        if term in healthy_foods:
+            healthy_food_sum += diff
+            healthy_food_count += 1
+        elif term in neutral_foods:
+            neutral_food_sum += diff
+            neutral_food_count += 1
+        else:
+            unhealthy_food_sum += diff
+            unhealthy_food_count += 1
+    return healthy_food_sum / healthy_food_count, \
+        neutral_food_sum / neutral_food_count, \
+        unhealthy_food_sum / unhealthy_food_count
 
 def main():
-    low_100_tf, top_100_tf = get_biggest_rank_change_terms(Tf_Plain_Col, lambda x: int(x), 100)
-    print_rank_change(top_100_tf, "Top", "tf")
-    print_rank_change(low_100_tf, "Bottom", "tf")
+    term_value_list_2018, term_to_rank_2018 = \
+        get_term_ranking("tf-idf-2018-3-1-thru-2019-9-1.txt", Tf_Plain_Col, \
+                         lambda x: int(x))
+    term_value_list_2020, term_to_rank_2020 = \
+        get_term_ranking("tf-idf-2020-3-1-thru-2021-9-1.txt", Tf_Plain_Col, \
+                         lambda x: int(x))
 
-    low_100_df, top_100_df = get_biggest_rank_change_terms(Df_Col, lambda x: float(x), 100)
+    terms_by_rank_diff = get_terms_by_rank_diff(term_to_rank_2018, term_to_rank_2020)
+
+    print_ranks(terms_by_rank_diff, "tf")
+
+    healthy_avg_diff, neutral_avg_diff, unhealthy_avg_diff = get_stats(terms_by_rank_diff)
+    print("Average rank difference for healthy food words:", healthy_avg_diff)
+    print("Average rank difference for neutral food words:", neutral_avg_diff)
+    print("Average rank difference for unhealthy food words:", unhealthy_avg_diff)
+
+    """low_100_df, top_100_df = get_biggest_rank_change_terms(Df_Col, lambda x: float(x), 100)
     print_rank_change(top_100_df, "Top", "df")
     print_rank_change(low_100_df, "Bottom", "df")
 
     low_100_tf_log_idf_log, top_100_tf_log_idf_log = \
         get_biggest_rank_change_terms(Tf_Log_Idf_Log_Col, lambda x: float(x), 100)
     print_rank_change(top_100_tf_log_idf_log, "Top", "(1+log(tf))*log(idf)")
-    print_rank_change(low_100_tf_log_idf_log, "Bottom", "(1+log(tf))*log(idf)")
-
-    #tf_ranking = get_food_words_rank_change_terms(Tf_Plain_Col, lambda x: int(x))
-    #print_rank_change(tf_ranking, "Food words", "tf")
-
-    #df_ranking = get_food_words_rank_change_terms(Df_Col, lambda x: float(x))
-    #print_rank_change(df_ranking, "Food words", "df")
-
-    #tf_log_idf_log_ranking = \
-    #    get_food_words_rank_change_terms(Tf_Log_Idf_Log_Col, lambda x: float(x))
-    #print_rank_change(tf_log_idf_log_ranking, "Food words", "(1+log(tf))*log(idf)")
+    print_rank_change(low_100_tf_log_idf_log, "Bottom", "(1+log(tf))*log(idf)")"""
 
 main()
