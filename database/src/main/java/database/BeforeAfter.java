f94febb5edb302ead884a4aa39734d1c7fbab484
csb6 <cblakley15@gmail.com>
2022-01-16 23:02:25 -0700

    Cleaned up BeforeAfter.java and changed from having a PMI column to having a PMI diff column

--- database/src/main/java/database/BeforeAfter.java
+++ database/src/main/java/database/BeforeAfter.java
@@ -29,20 +29,25 @@ import org.apache.lucene.util.BytesRef;
 
 public class BeforeAfter {
 
-    public static class DocFrequencyCollector extends SimpleCollector {
+    public static class TermStatsCollector extends SimpleCollector {
         public IndexReader reader;
+        // Maps terms -> the number of documents (in this selection) that
+        // they appear in
         public Map<String, Long> termToDocFreq = new HashMap<>(1000);
+        // Maps terms -> the number of times the term occurs across all
+        // documents (in this selection)
         public Map<String, Long> termToTermFreq = new HashMap<>(1000);
-        public int totalTweetsInPeriod = 0;
+        // The number of tweets (documents) in the selection
+        public int totalTweets = 0;
 
-        public DocFrequencyCollector(IndexReader reader) {
+        public TermStatsCollector(IndexReader reader) {
             this.reader = reader;
         }
 
         @Override
         public void collect(int doc) throws IOException {
             Terms terms = reader.getTermVector(doc, "text");
-            ++totalTweetsInPeriod;
+            ++totalTweets;
             TermsEnum termsInDoc = terms.iterator();
             BytesRef currTermInDoc = termsInDoc.next();
             while (currTermInDoc != null) {
@@ -110,29 +115,32 @@ public class BeforeAfter {
 
     public static void printTermStats(String filename,
             Map<String, Long> termToDocFreq, Map<String, Long> termToTermFreq,
-            Map<String, Double> termToPMI, int totalTweetsInPeriod)
+            Map<String, Double> termToPMIDiff, int totalTweetsInPeriod)
             throws IOException {
         FileWriter outputFile = new FileWriter(filename);
         for (Entry<String, Long> entry : termToDocFreq.entrySet()) {
             String term = entry.getKey();
             long tfPlain = termToTermFreq.get(term);
-            if (tfPlain <= 5) {
+            if (tfPlain <= 15 || !termToPMIDiff.containsKey(term)) {
+                // Ignore rare terms or terms that only appear in one of the
+                // periods
                 continue;
             }
             double tfLog = 1 + Math.log10(tfPlain);
             long df = entry.getValue();
             double idfPlain = (double) totalTweetsInPeriod / (double) df;
             double idfLog = Math.log10(idfPlain);
-            double pmi = termToPMI.get(term);
+            double pmiDiff = termToPMIDiff.get(term);
             outputFile.write(term + "\t" + tfPlain + "\t" + tfLog + "\t" + df
                     + "\t" + idfPlain + "\t" + idfLog + "\t"
                     + tfPlain * idfPlain + "\t" + tfPlain * idfLog + "\t"
-                    + tfLog * idfPlain + "\t" + tfLog * idfLog + "\t" + pmi);
+                    + tfLog * idfPlain + "\t" + tfLog * idfLog + "\t" + pmiDiff
+                    + "\n");
         }
         outputFile.close();
     }
 
-    public static DocFrequencyCollector collectPeriod(Analyzer analyzer,
+    public static TermStatsCollector collectPeriod(Analyzer analyzer,
             Directory index, String periodQueryText)
             throws ParseException, IOException {
         Query periodQuery = new QueryParser("postTime", analyzer)
@@ -141,7 +149,7 @@ public class BeforeAfter {
         IndexReader reader = DirectoryReader.open(index);
         IndexSearcher searcher = new IndexSearcher(reader);
 
-        DocFrequencyCollector collector = new DocFrequencyCollector(reader);
+        TermStatsCollector collector = new TermStatsCollector(reader);
         searcher.search(periodQuery, collector);
 
         return collector;
@@ -162,17 +170,32 @@ public class BeforeAfter {
         return result;
     }
 
-    public static Map<String, Double> collectPMI(DocFrequencyCollector period,
+    public static Map<String, Double> collectPMI(TermStatsCollector period,
             Map<String, Long> allPeriods) {
         Map<String, Double> result = new HashMap<>(period.termToDocFreq.size());
-        long tweetsInPeriod = period.totalTweetsInPeriod;
         for (Entry<String, Long> entry : period.termToDocFreq.entrySet()) {
             String term = entry.getKey();
             long selectedTweetsInPeriod = entry.getValue();
             long totalSelectedTweets = allPeriods.get(term);
 
             result.put(term, getPMI(selectedTweetsInPeriod, totalSelectedTweets,
-                    tweetsInPeriod));
+                    period.totalTweets));
+        }
+        return result;
+    }
+
+    public static Map<String, Double> collectPMIDiff(
+            Map<String, Double> period1PMI, Map<String, Double> period2PMI) {
+        Map<String, Double> result = new HashMap<>(2000);
+        for (Entry<String, Double> entry : period1PMI.entrySet()) {
+            String term = entry.getKey();
+            double period1PMIValue = entry.getValue();
+            Double period2PMIValue = period2PMI.get(term);
+            if (period2PMIValue == null) {
+                // Term isn't found in period2; ignore it
+                continue;
+            }
+            result.put(term, period2PMIValue - period1PMIValue);
         }
         return result;
     }
@@ -185,30 +208,32 @@ public class BeforeAfter {
         String period2QueryText = "[2020-03-01T00:00:00 TO 2021-09-01T23:59:59]";
 
         System.out.println("Collecting data for " + period1QueryText);
-        DocFrequencyCollector period1Collector = collectPeriod(analyzer, index,
+        TermStatsCollector period1 = collectPeriod(analyzer, index,
                 period1QueryText);
         System.out.println("Collecting data for " + period2QueryText);
-        DocFrequencyCollector period2Collector = collectPeriod(analyzer, index,
+        TermStatsCollector period2 = collectPeriod(analyzer, index,
                 period2QueryText);
 
         System.out.println("Collecting data for all periods");
         Map<String, Long> allPeriodsDocFrequency = collectTotal(
-                period1Collector.termToDocFreq, period2Collector.termToDocFreq);
+                period1.termToDocFreq, period2.termToDocFreq);
 
         System.out.println("Collecting PMI for " + period1QueryText);
-        Map<String, Double> period1PMI = collectPMI(period1Collector,
+        Map<String, Double> period1PMI = collectPMI(period1,
                 allPeriodsDocFrequency);
         System.out.println("Collecting PMI for " + period2QueryText);
-        Map<String, Double> period2PMI = collectPMI(period2Collector,
+        Map<String, Double> period2PMI = collectPMI(period2,
                 allPeriodsDocFrequency);
+
+        Map<String, Double> pmiDiff = collectPMIDiff(period1PMI, period2PMI);
 
         System.out.println("Writing results to file");
         printTermStats("tf-idf-2018-3-1-thru-2019-9-1.txt",
-                period1Collector.termToDocFreq, period1Collector.termToTermFreq,
-                period1PMI, period1Collector.totalTweetsInPeriod);
+                period1.termToDocFreq, period1.termToTermFreq, pmiDiff,
+                period1.totalTweets);
         printTermStats("tf-idf-2020-3-1-thru-2021-9-1.txt",
-                period2Collector.termToDocFreq, period2Collector.termToTermFreq,
-                period2PMI, period2Collector.totalTweetsInPeriod);
+                period2.termToDocFreq, period2.termToTermFreq, pmiDiff,
+                period2.totalTweets);
     }
 
 }
